26d44e911aa90875592d742ebca57d667adc92b7
Nasoavina <nasoavina.m@gmail.com>
2025-04-25 12:30:14 +0200

    header: update

--- components/Header.tsx
+++ components/Header.tsx
@@ -10,9 +10,9 @@ export default function Header() {
           Ai-Cover
         </h1>
       </Link>
-      <p className="border rounded-2xl py-1 px-4 text-slate-500 text-sm hover:scale-105 transition duration-300 ease-in-out">
+      {/* <p className="border rounded-2xl py-1 px-4 text-slate-500 text-sm hover:scale-105 transition duration-300 ease-in-out">
         <b>48,325</b> lettres de motivation générées
-      </p>
+      </p> */}
     </header>
   );
 }
